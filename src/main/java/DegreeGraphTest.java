51d60ff78538af5139a2f7ffd01dfeaf7590a1eb
Nathanael Sharples <shany068@mymail.unisa.edu.au>
2026-06-06 15:03:35 +1000

    Added compulsory header comments to the DegreeGraphTest class.

--- src/main/java/DegreeGraphTest.java
+++ src/main/java/DegreeGraphTest.java
@@ -1,3 +1,14 @@
+/**
+ * File: DegreeGraphTest.java
+ * Description: 11 tests for testing the methods of the DegreeGraph class.
+ * Author: Nathanael Sharples
+ * Student ID: a2983892
+ * Email ID: a2983892@adelaide.edu.au
+ * AI Tool Used: Yes. Chat gpt was used for research and help with some code logic.
+ * This is my own work as defined by
+ *    the University's Academic Integrity Policy.
+ **/
+
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
